93f61ad047518d8637a1978f64db42229d64d8a7
Mohamed Khalil Azzabi <azzabimedkhalil@gmail.com>
2024-11-16 13:36:35 +0100

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -5,37 +5,48 @@ from google.colab import drive
 drive.mount('/content/drive')
 
 
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = (5.0, 4.0)  # Set default size of plots
+plt.rcParams['figure.figsize'] = (5.0, 4.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 np.random.seed(1)
 
-# Load and preprocess data (adjust according to your dataset)
+# Load and preprocess data
 def load_data():
     train_dataset = h5py.File('train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # Train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # Train set labels
+    test_dataset = h5py.File('test_catvnoncat.h5', "r")
+
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # Training set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # Training set labels
+
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])     # Test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])     # Test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])           # List of classes
 
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
 
-    return train_set_x_orig, train_set_y_orig
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-train_x_orig, train_y = load_data()
+# Load data
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
 # Flatten and normalize
-train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T  # Shape (num_px*num_px*3, m)
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T  # Shape: (12288, m_train)
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T     # Shape: (12288, m_test)
+
 train_x = train_x_flatten / 255.
+test_x = test_x_flatten / 255.
 
-# Define layer dimensions
-layers_dims = [train_x.shape[0], 20, 7, 5, 1]  # Example layer dimensions
+n_x = train_x.shape[0]
+n_y = 1
 
+# Define activation functions and their derivatives
 def sigmoid(Z):
     A = 1 / (1 + np.exp(-Z))
     cache = Z
@@ -54,19 +65,24 @@ def sigmoid_backward(dA, cache):
 
 def relu_backward(dA, cache):
     Z = cache
-    dZ = np.array(dA, copy=True)
+    dZ = np.array(dA, copy=True)  # Convert dA to a correct object.
     dZ[Z <= 0] = 0
     return dZ
 
-def initialize_parameters_deep(layer_dims):
-    np.random.seed(3)
-    parameters = {}
-    L = len(layer_dims)
-
-    for l in range(1, L):
-        parameters['W' + str(l)] = (np.random.randn(layer_dims[l], layer_dims[l - 1]) *
-                                    np.sqrt(2 / layer_dims[l - 1]))  # He initialization
-        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+# Define initialization and forward/backward functions
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(1)
+    
+    W1 = np.random.randn(n_h, n_x) * np.sqrt(2. / n_x)
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * np.sqrt(2. / n_h)
+    b2 = np.zeros((n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
     return parameters
 
 def linear_forward(A, W, b):
@@ -81,37 +97,13 @@ def linear_activation_forward(A_prev, W, b, activation):
     elif activation == "relu":
         Z, linear_cache = linear_forward(A_prev, W, b)
         A, activation_cache = relu(Z)
-
+    
     cache = (linear_cache, activation_cache)
     return A, cache
 
-def L_model_forward(X, parameters):
-    caches = []
-    A = X
-    L = len(parameters) // 2  # Number of layers
-
-    # [LINEAR -> RELU]*(L-1)
-    for l in range(1, L):
-        A_prev = A
-        A, cache = linear_activation_forward(A_prev,
-                                             parameters['W' + str(l)],
-                                             parameters['b' + str(l)],
-                                             activation='relu')
-        caches.append(cache)
-
-    # LINEAR -> SIGMOID
-    AL, cache = linear_activation_forward(A,
-                                          parameters['W' + str(L)],
-                                          parameters['b' + str(L)],
-                                          activation='sigmoid')
-    caches.append(cache)
-
-    assert(AL.shape == (1, X.shape[1]))
-    return AL, caches
-
 def compute_cost(AL, Y):
     m = Y.shape[1]
-    epsilon = 1e-15  # To prevent log(0)
+    epsilon = 1e-15  # Small value to prevent log(0)
     cost = (-1 / m) * np.sum(Y * np.log(AL + epsilon) + (1 - Y) * np.log(1 - AL + epsilon))
     cost = np.squeeze(cost)
     return cost
@@ -119,116 +111,44 @@ def compute_cost(AL, Y):
 def linear_backward(dZ, cache):
     A_prev, W, b = cache
     m = A_prev.shape[1]
-
+    
     dW = (1 / m) * np.dot(dZ, A_prev.T)
     db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
-
+    
     return dA_prev, dW, db
 
 def linear_activation_backward(dA, cache, activation):
     linear_cache, activation_cache = cache
-
+    
     if activation == "relu":
         dZ = relu_backward(dA, activation_cache)
     elif activation == "sigmoid":
         dZ = sigmoid_backward(dA, activation_cache)
-
+    
     dA_prev, dW, db = linear_backward(dZ, linear_cache)
     return dA_prev, dW, db
 
-def L_model_backward(AL, Y, caches):
-    grads = {}
-    L = len(caches)
-    m = AL.shape[1]
-    Y = Y.reshape(AL.shape)
-
-    # Initialize backpropagation
-    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-
-    # Lth layer (SIGMOID -> LINEAR)
-    current_cache = caches[L - 1]
-    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(
-        dAL, current_cache, activation="sigmoid")
-
-    # Loop from l=L-2 to l=0
-    for l in reversed(range(L - 1)):
-        current_cache = caches[l]
-        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(
-            grads["dA" + str(l + 2)], current_cache, activation="relu")
-        grads["dA" + str(l + 1)] = dA_prev_temp
-        grads["dW" + str(l + 1)] = dW_temp
-        grads["db" + str(l + 1)] = db_temp
-
-    return grads
-
 def update_parameters(parameters, grads, learning_rate):
     L = len(parameters) // 2  # Number of layers
-
-    for l in range(L):
-        parameters["W" + str(l + 1)] -= learning_rate * grads["dW" + str(l + 1)]
-        parameters["b" + str(l + 1)] -= learning_rate * grads["db" + str(l + 1)]
+    
+    for l in range(1, L + 1):
+        parameters["W" + str(l)] -= learning_rate * grads["dW" + str(l)]
+        parameters["b" + str(l)] -= learning_rate * grads["db" + str(l)]
     return parameters
 
-def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
-    np.random.seed(1)
-    costs = []
-
-    # Parameters initialization
-    parameters = initialize_parameters_deep(layers_dims)
-
-    # Loop (gradient descent)
-    for i in range(num_iterations):
-        # Forward propagation
-        AL, caches = L_model_forward(X, parameters)
-
-        # Compute cost
-        cost = compute_cost(AL, Y)
-
-        # Backward propagation
-        grads = L_model_backward(AL, Y, caches)
-
-        # Update parameters
-        parameters = update_parameters(parameters, grads, learning_rate)
-
-        # Print the cost every 100 iterations
-        if print_cost and i % 100 == 0:
-            print("Cost after iteration {}: {}".format(i, cost))
-            costs.append(cost)
-
-    # Plot the cost
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('Cost')
-    plt.xlabel('Iterations (per hundreds)')
-    plt.title("Learning rate = {}".format(learning_rate))
-    plt.show()
-
-    return parameters
-
-def predict(X, y, parameters):
-    m = X.shape[1]
-    n = len(parameters) // 2
-    p = np.zeros((1, m))
-
+def predict(X, Y, parameters):
+    L = len(parameters) // 2
+    A = X
+    
     # Forward propagation
-    probas, caches = L_model_forward(X, parameters)
-
-    # Convert probabilities to 0/1 predictions
-    for i in range(0, probas.shape[1]):
-        if probas[0, i] > 0.5:
-            p[0, i] = 1
-        else:
-            p[0, i] = 0
-
-    # Print accuracy
-    accuracy = np.sum((p == y) / m)
-    print("Accuracy: " + str(accuracy))
-
-    return p
-
-# Train the model
-parameters = L_layer_model(train_x, train_y, layers_dims, learning_rate=0.0075,
-                           num_iterations=2500, print_cost=True)
-
-# Predict on training set
-predictions_train = predict(train_x, train_y, parameters)
+    for l in range(1, L):
+        A_prev = A
+        A, _ = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], 'relu')
+    
+    AL, _ = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], 'sigmoid')
+    predictions = (AL > 0.5).astype(int)
+    
+    accuracy = np.mean(predictions == Y) * 100
+    print(f"Accuracy: {accuracy:.2f}%")
+    return predictions
